e343094778e7d25791b80fb1f7640246203d54bc
stevejk31 <stevekim31@gmail.com>
2016-05-13 17:15:14 -0700

    problems with loading model to page

--- polls/urls.py
+++ polls/urls.py
@@ -13,10 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
 from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
+    # ex: /polls/
     url(r'^$', views.index, name='index'),
+    # ex: /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
